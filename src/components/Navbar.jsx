471a96679440c02e7e9cfc77e76745e3d09ffe07
Hatice-Altun <van>
2023-04-09 01:14:59 +0200

    Added Responsive Navbar Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,47 @@
-import React, { useState } from "react";
-import Logo from "../assets/Logo.png";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import ReorderIcon from "@material-ui/icons/Reorder";
 import {ShoppingBag} from "phosphor-react";
+import Logo from "../assets/Logo.png";
 import "../styles/Navbar.css";
 
 function Navbar() {
-  const [openLinks, setOpenLinks] = useState(false);
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
 
-  const toggleNavbar = () => {
-    setOpenLinks(!openLinks);
-  };
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
 
   return (
-    <div className="navbar">
-      <div className="leftSide" id={openLinks ? "open" : "close"}>
+      <div className="navbar" id={expandNavbar ? "open" : "close"}>
 
-        <div className="link">
-        <Link to="/">
-          <span className="logo-container">
-            <img src={Logo} alt="logo"/>
-          </span>
-        </Link>
+        <div className="toggleButton">
+          <button
+              onClick={() => {
+                setExpandNavbar((prev) => !prev);
+              }}
+          >
+            <ReorderIcon />
+          </button>
         </div>
 
-        <div className="hiddenLinks">
-          <Link to="/"> Home </Link>
-          <Link to="/shop"> Macarons </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/contact"> Contact </Link>
-          <Link to="/cart">
-            <ShoppingBag size={35}/>
+        <div className="links">
+          <Link to="/">
+                <span className="logo-container">
+                    <img src={Logo} alt="logo"/>
+                </span>
           </Link>
+          <Link to="/"> Home </Link>
+          <Link to="/About"> About </Link>
+          <Link to="/Shop"> Shop </Link>
+          <Link to="/Contact"> Contact </Link>
+            <Link to="/cart">
+                <ShoppingBag size={35}/>
+            </Link>
         </div>
-      </div>
-
-      <div className="rightSide">
-        <Link to="/"> Home </Link>
-        <Link to="/shop"> Shop </Link>
-        <Link to="/about"> About </Link>
-        <Link to="/contact"> Contact </Link>
-        <Link to="/cart">
-          <ShoppingBag size={35}/>
-        </Link>
-        <button onClick={toggleNavbar}>
-          <ReorderIcon />
-        </button>
-      </div>
     </div>
+
   );
 }
 
